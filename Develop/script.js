10d02f7a564d549b4703c5c31b249514567667b1
tteklu <teamirteklu@gmail.com>
2023-02-20 12:04:29 -0700

    creating dynamic html checkbox criteria

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,10 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+// var generateCriteria = document.querySelectorAll("#input[name=checkbox]");
+var generateCriteria = document.querySelector("#criteria");
+var passwordLength = document.querySelector("#passlen");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -15,3 +19,29 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+generateCriteria.addEventListener("click", function () {
+var checkbox = document.createElement('input');
+checkbox.type = "checkbox";
+checkbox.name = "name";
+checkbox.value = "value";
+checkbox.id = "id";
+
+var label = document.createElement('label')
+label.htmlFor = "id";
+label.appendChild(document.createTextNode('text for label after checkbox'));
+
+container.appendChild(checkbox);
+container.appendChild(label);
+
+});
+
+// generateCriteria.forEach(function(checkbox) {
+//   checkbox.addEventListener('change', function() {
+//     var checkedBoxes = [];
+
+//     checkedBoxes = Array.from(generateCriteria).filter(i => i.checked).map(i => i.value);
+
+//     console.log(checkedBoxes);
+//   });
+// });
