ce3907f3c03992a91e390b058e0a02f864401b62
tteklu <teamirteklu@gmail.com>
2023-02-22 16:08:19 -0700

    added submit and password length criteria

--- Develop/script.js
+++ Develop/script.js
@@ -3,25 +3,21 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-// var generateCriteria = document.querySelectorAll("#input[name=checkbox]");
-// var generateCriteria = document.querySelector("#criteria");
-// var passwordLength = document.querySelector("#passlen");
+var generateSubmit = document.querySelector("#button");
+var passwordLength = document.querySelector("#passlen");
 
 const criteria = ["Lowercase", "Uppercase", "Numbers", "Special Characters"];
 // Write password to the #password input
+
+
 function writePassword() {
+var checkboxes = document.getElementsByName("criteria");
 
-var checkboxes = document.getElementById("criteria")
+console.log(checkboxes);
 
-
-  criteria.forEach((checkbox) => {
-    console.log(checkbox);
-
-    if (criteria[] = ischecked) {
-      console.log("yes");
-    }
-  });
+if(checkboxes[0].checked == true) {
   
+} 
 
 
 
@@ -31,23 +27,20 @@ var checkboxes = document.getElementById("criteria")
 }
 
 function writeCriteria() {
-  
-
   criteria.forEach((element) => {
 
     const id = `element: ${element}`;
-
-    
 
   const label = document.createElement('label');
   label.setAttribute('for', id);
 
   var checkbox = document.createElement('input');
   checkbox.type = "checkbox";
-  checkbox.name = "name";
+  checkbox.name = "criteria";
   checkbox.value = element;
   checkbox.id = id;
 
+  
   label.appendChild(checkbox);
   label.appendChild(document.createTextNode(element));
 
@@ -55,12 +48,38 @@ function writeCriteria() {
   
 
   document.querySelector("#criteria").appendChild(label);
-
  });
+
+ const passLabel = document.createElement('label');
+ passLabel.setAttribute('for', "passlen");
+
+  var passLength = document.createElement('input');
+
+  passLength.type = "number";
+  passLength.name = "passlen"
+  passLength.value = "passwordLength";
+  passLength.id = "passlen";
+  passLength.min = "8";
+  passLength.max = "128";
+
+  passLabel.appendChild(passLength);
+  passLabel.appendChild(document.createTextNode("Password Length (8-128)"));
+
+  document.querySelector('#passlen').appendChild(passLength);
+  document.querySelector('#passlen').appendChild(passLabel);
+
+  var button = document.createElement('input');
+  button.type = "submit";
+  button.name = "button";
+  button.value = "submitCriteria";
+  button.id = "button";
+
+  document.querySelector("#button").appendChild(button);
+
+
 }
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 generateBtn.addEventListener("click", writeCriteria);
-
+generateSubmit.addEventListener("click", writePassword);
